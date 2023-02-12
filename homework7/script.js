72250aaf644e956504e82ad4aa65b2c43fa38ab0
Ivan Agaev <ivanagaev044@gmail.com>
2023-02-12 20:32:42 +0200

    task7

---
+++ homework7/script.js
@@ -0,0 +1,5 @@
+let firstNum = prompt('Введіть перше число');
+let secondNum = prompt('Введіть друге число');
+let thirdNum = prompt('Введіть трєте число');
+let result = (Number(firstNum) + Number(secondNum) + Number(thirdNum)) / 3;
+alert(result + ' - середне арифмитичне');
