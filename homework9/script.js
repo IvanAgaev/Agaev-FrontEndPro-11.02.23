881b7b99daa70c0f659cc398d5ce65946153a97d
Ivan Agaev <ivanagaev044@gmail.com>
2023-02-12 21:49:53 +0200

    task9

---
+++ homework9/script.js
@@ -0,0 +1,19 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch(true){
+
+    case numOrStr===null:
+        console.log('ви скасували');
+        break;
+
+    case numOrStr.trim() ==='':
+        console.log('Empty String');
+        break;
+
+     case isNan(+numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+        default:
+            console.log('OK!');
+}
