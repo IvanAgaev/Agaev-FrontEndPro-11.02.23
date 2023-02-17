53772d4e9a986aee2f01b33ee4d9fec6cbab60fc
Ivan Agaev <ivanagaev044@gmail.com>
2023-02-17 13:29:33 +0200

    home8

--- homework8/script.js
+++ homework8/script.js
@@ -1,131 +1,49 @@
-let birthDate = prompt('Введіть рік свого народження.');
+const birthDate = prompt(`Якого ти року народження?`);
+if (isNaN(Number(birthDate))) {
+    alert(`Помилка! Дата має бути числом`);
+} else if (birthDate === null || birthDate.trim() === '') {
+    alert(`Шкода, що ти не захотів заповнити форму`);
+} else {
+    const homeTown = prompt(`З якого ти міста?`);
+    if (homeTown === null || homeTown.trim() === '') {
+        alert('Шкода, що ти не захотів заповнити форму');
+    } else {
+        const favSport = prompt(`Введіть свій улюблений спорт`);
+        if (favSport === null || favSport.trim() === '') {
+            alert('Шкода, що ти не захотів заповнити форму');
+        } else {
+            const age = 2023 - Number(birthDate);
+            let ageStr = `Тобі ${age} років`;
+            let cityStr;
+            let sportStr;
+            
+            if (homeTown === 'Київ') {
+                cityStr = 'Ти живеш у столиці України';
+            } else if (homeTown === 'Лондон') {
+                cityStr = 'Ти живеш у столиці Великобританії';
+            } else if (homeTown === 'Вашингтон') {
+                cityStr = 'Ти живеш у столиці США';
+            } else {
+                cityStr = `Ти живеш у місті ${homeTown}`;
+            }
 
-if (birthDate === null){
-    alert("шкода, що ви не захотіли вказати свій вік");
-}
+            if (favSport === 'Шахи') {
+                sportStr = `Круто! \n Хочеш стати новим Магнусом Карлсеном?`;
+            } else if (favSport === 'Футбол') {
+                sportStr = `Круто! \n Хочеш стати новим Мессі?`;
+            } else if (favSport === 'Баскетбол') {
+                sportStr = `Круто! \n Хочеш стати новим Джорданом?`
+            } else {
+                sportStr = `Твій улюблений спорт - ${favSport}`;
+            }
+            alert (`${ageStr} \n ${cityStr} \n ${sportStr}`);
+            
+        }
 
-
-else{
-let birthDateTrim = birthDate.trim();
-if(isNaN(+birthDate) || birthDateTrim ===''){
-    alert('Невірно введено дату. Дата має бути обов`язково заповнена числовим значенням.');
-}
-else{
- 
-let homeTown = prompt('В якому місті ви проживаєте?');
-if (homeTown === null){
-    alert("шкода, що ви не захотіли вказати своє місто");
-}
-else{ 
- let homeTownTrim = homeTown.trim() ;
- if(homeTownTrim ===''){
-    alert('Рядок місто має бути заповненим');
- }  
- else{
-let favSport = prompt('Вкажіть ваш улюблений вид спорту.');
-if (favSport === null){
-    alert("шкода, що ви не захотіли вказати свій улюблений спорт");
-}
-else{
-let favSportTrim = favSport.trim();
-if(favSportTrim ===''){
-    alert('Рядок улюблений спорт має бути заповненим');
-}
-else{
-let ukraine = 'України';
-let usa = 'США';
-let space = ' ';
-let britain = 'Великобританії';
-
-let userAge = 2023 - Number(birthDate);
-
-
-if(homeTown === 'Київ' && favSport === 'шахи'){
-alert('Тобі' + space + userAge + space + 'років' +'\n'
-       + 'Ти живеш у столиці' + space + ukraine + '\n'
-       + 'Круто! Хочеш стати Магнусом Карлсеном?' );
-}
-else if(homeTown === 'Лондон' && favSport==='шахи'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + britain + '\n'
-           + 'Круто! Хочеш стати Магнусом Карлсеном?' );
-}
-           else if(homeTown === 'Вашингтон' && favSport==='шахи'){
-            alert('Тобі' + space + userAge + space + 'років' +'\n'
-                   + 'Ти живеш у столиці' + space + usa + '\n'
-                   + 'Круто! Хочеш стати Магнусом Карлсеном?' );
-}
-else if(homeTown === 'Лондон' && favSport==='футбол'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + britain + '\n'
-           + 'Круто! Хочеш стати Ліонелем Мессі?' );
-}
-else if(homeTown === 'Київ' && favSport==='футбол'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + ukraine + '\n'
-           + 'Круто! Хочеш стати Ліонелем Мессі?' );
-}
-           else if(homeTown === 'Вашингтон' && favSport==='футбол'){
-            alert('Тобі' + space + userAge + space + 'років' +'\n'
-                   + 'Ти живеш у столиці' + space + usa + '\n'
-                   + 'Круто! Хочеш стати Майклом Джорданом?' );
-}
-else if(homeTown === 'Лондон' && favSport==='баскетбол'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + britain + '\n'
-           + 'Круто! Хочеш стати Майклом Джорданом?' );
-}
-else if(homeTown === 'Київ' && favSport==='баскетбол'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + ukraine + '\n'
-           + 'Круто! Хочеш стати Майклом Джорданом?' );
-}
-           else if(homeTown === 'Вашингтон' && favSport==='баскетбол'){
-            alert('Тобі' + space + userAge + space + 'років' +'\n'
-                   + 'Ти живеш у столиці' + space + usa + '\n'
-                   + 'Круто! Хочеш стати Майклом Джорданом?' );
+    }
 }
 
-else if(favSport==='баскетбол'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у місті' + space + homeTown + '\n'
-           + 'Круто! Хочеш стати Майклом Джорданом?' );
-}
-           else if(favSport==='футбол'){
-            alert('Тобі' + space + userAge + space + 'років' +'\n'
-                   + 'Ти живеш у місті' + space + homeTown + '\n'
-                   + 'Круто! Хочеш стати Ліонелем Мессі?' );
-}
-else if(favSport==='шахи'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у місті' + space + homeTown + '\n'
-           + 'Круто! Хочеш стати Магнусом Карлсеном?' );
-}
-else if(homeTown === 'Київ'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-    + 'Ти живеш у столиці' + space + ukraine + '\n'
-    + 'Твій улюблений вид спорту' + space + favSport );
-}
-           else if(homeTown === 'Вашингтон'){
-            alert('Тобі' + space + userAge + space + 'років' +'\n'
-            + 'Ти живеш у столиці' + space + usa + '\n'
-            + 'Твій улюблений вид спорту' + space + favSport );
-}
-else if(homeTown === 'Лондон'){
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-           + 'Ти живеш у столиці' + space + britain + '\n'
-           + 'Твій улюблений вид спорту' + space + favSport );
-}
-else{
-    alert('Тобі' + space + userAge + space + 'років' +'\n'
-    + 'Ти живеш у місті' + space + homeTown + '\n'
-    + 'Твій улюблений вид спорту' + space + favSport ); 
-}
-}
-}
-}
-}
-}
-}
+
+
 
 
