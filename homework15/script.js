8cdc7a7d653f05dbd97a0535ca944b218ca951e4
Ivan Agaev <ivanagaev044@gmail.com>
2023-02-20 12:00:45 +0200

    splice

--- homework15/script.js
+++ homework15/script.js
@@ -1,17 +1,22 @@
 /*Реалізуйте функцію removeElement(array, item),
  щоб видалити елемент item з масиву array. */
 
- const array = [1, 2, 3, 4, 5, 6, 7];
+ const array = [1, 2, 3, 4, 5, 6,5, 7];
+
+ alert(`Old array
+ ${array}`);
+ 
  const removeFunction = (array,item) => {
-    const modifiedArray = array.filter((element) => element !== item);
-    return modifiedArray;
+    array.map((element,index,array) => {
+     if (element === item) {
+      array.splice(index,1);
+     }});
+    return array;
  };
  
  const result = removeFunction(array,5);
 
- alert(`Old array
-  ${array}
- New array with deleted item 5
+ alert(`New array with deleted item 5
   ${result}`);
 
  
