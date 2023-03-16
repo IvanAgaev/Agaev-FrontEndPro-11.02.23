fb707bc746c8925a2c775303be790f69af4005c8
IvanAgaev <ivanagaev044@gmail.com>
2023-03-16 21:25:52 +0200

    task33

--- homework33/main.js
+++ homework33/main.js
@@ -1,16 +1,26 @@
 const array = [1,2,3,5,[8,9,10], 'Ваня', true];
 
-
 const generateList = (arr) => {
     const p = document.createElement('ul');
     document.body.prepend(p);
     for (let i = 0; i<arr.length; i++) {
+        if(Array.isArray(arr[i])){
+            let newLiParent = document.createElement('li');
+            let newUl = document.createElement('ul');
+            for (let j = 0; j<arr[i].length;j++) {
+                let newLiinUl = document.createElement('li');
+                newLiinUl.innerHTML = arr[i][j];
+                newUl.append(newLiinUl);
+                newLiParent.append(newUl);
+                p.append(newLiParent);
+            }
+
+        } else {
         let newLi = document.createElement('li');
         newLi.innerHTML = arr[i];
         p.append(newLi);
-
-
     }
-
+}
 };
+
 generateList(array);
