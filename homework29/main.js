43faac855d19cd0e4f1cd086a631a9b8762cea8d
IvanAgaev <ivanagaev044@gmail.com>
2023-03-17 09:56:21 +0200

    refactor task 29

--- homework29/main.js
+++ homework29/main.js
@@ -7,12 +7,14 @@ const handlerFirst = () => {
     if (!link1.href.startsWith('http')) {
         link1.href = 'http://' + link1.href;
     }
+    
     window.location.href = link1.href;
 };
 const handlerSecond = () => {
     if (!link2.href.startsWith('https')) {
         link2.href = 'https://' + link2.href;
     }
+
     window.location.href = link2.href;
 };
 
