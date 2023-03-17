b3fc9adfe878094b282e0384a8bced06bf04db91
IvanAgaev <ivanagaev044@gmail.com>
2023-03-17 10:22:32 +0200

    refactor task 33

--- homework33/main.js
+++ homework33/main.js
@@ -1,26 +1,19 @@
 const array = [1,2,3,5,[8,9,10], 'Ваня', true];
 
 const generateList = (arr) => {
-    const p = document.createElement('ul');
-    document.body.prepend(p);
-    for (let i = 0; i<arr.length; i++) {
-        if(Array.isArray(arr[i])){
-            let newLiParent = document.createElement('li');
-            let newUl = document.createElement('ul');
-            for (let j = 0; j<arr[i].length;j++) {
-                let newLiinUl = document.createElement('li');
-                newLiinUl.innerHTML = arr[i][j];
-                newUl.append(newLiinUl);
-                newLiParent.append(newUl);
-                p.append(newLiParent);
-            }
-
+    const ul = document.createElement('ul');
+    arr.forEach(item => {
+        let li = document.createElement('li');
+        if(Array.isArray(item)) {
+           li.append(generateList(item));
         } else {
-        let newLi = document.createElement('li');
-        newLi.innerHTML = arr[i];
-        p.append(newLi);
-    }
-}
-};
+            li.textContent = item;
+        }
 
-generateList(array);
+        ul.append(li);
+    });
+
+    return ul;
+}
+
+document.body.prepend(generateList(array));
