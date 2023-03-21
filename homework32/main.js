c17cff7c236681217bedb6dab069f465759137c3
IvanAgaev <ivanagaev044@gmail.com>
2023-03-21 14:43:18 +0200

    32 task changed

--- homework32/main.js
+++ homework32/main.js
@@ -1,30 +1,34 @@
-let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 const btnNext = document.getElementById('Next');
 const btnPrev = document.getElementById('prev');
+const images = document.querySelectorAll('img');
+let index = 0;
+
+const showImage = (index) => {
+    images.forEach(image => image.classList.remove('active'));
+    images[index].classList.add('active');
+    if (index===0) {
+        btnPrev.setAttribute('disabled', 'true');
+    } else {
+        btnPrev.removeAttribute('disabled');
+    }
+
+    if (index === images.length-1) {
+        btnNext.setAttribute('disabled', 'true');
+    } else {
+        btnNext.removeAttribute('disabled');
+    }
+
+}
 
 btnNext.addEventListener('click', (event) => {
-    offset+=530;
-    if (offset>530) {
-        btnNext.style.display = 'none';
-    } else if (offset = 530) {
-        btnPrev.style.display = 'block';
-    }
-    sliderLine.style.left = -offset + 'px';
-
-
+   index++;
+   showImage(index);
+ 
 
 });
 
 btnPrev.addEventListener('click', (event) => {
-    offset-=530;
-    if (offset<530) {
-        btnPrev.style.display = 'none';
-    } else if (offset = 530) {
-        btnNext.style.display = 'block';
-    }
-    sliderLine.style.left = -offset + 'px';
-
-
-
+  index--;
+  showImage(index);
 });
