c923a2b3762e2ef2486c5904fbd2936c55513a3d
IvanAgaev <ivanagaev044@gmail.com>
2023-05-14 23:17:50 +0300

    first commit

---
+++ homework40/src/Navbar.jsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+    return (
+        <ul className="navbar">
+            <li className="navlink"><a href="#" className="navitem active">Main Page</a></li>
+            <li className="navlink"><a href="#" className="navitem">About us</a></li>
+            <li className="navlink"><a href="#" className="navitem">Contacts</a></li>
+            <li className="navlink"><a href="#" className="navitem">Examples</a></li>
+        </ul>
+    )
+}
