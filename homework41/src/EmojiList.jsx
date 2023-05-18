6a03ba80a9fe4aa7c218233b516b3bf5ae94b488
IvanAgaev <ivanagaev044@gmail.com>
2023-05-18 21:35:58 +0300

    first

---
+++ homework41/src/EmojiList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Emoji from "./Emoji";
+
+const EmojiList = ({ emojis, incrementCount }) => {
+    
+    if (!Array.isArray(emojis)) {
+
+        return; 
+    }
+
+    return (
+        emojis.map(emoji => 
+             <Emoji onClick={(e) => { e.preventDefault(); incrementCount(emoji.id)}} count={emoji.count} imageCode={emoji.imageCode} key={emoji.id} />
+        )
+    )
+}
+
+export default EmojiList;
